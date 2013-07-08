1dff964d0b60f7d2d92b184c10f09120d1f7257d
Frederic Junod <frederic.junod@camptocamp.com>
2013-07-08 14:55:11 +0200

    Drag/drop files snapshot

--- ol/dropbox.js
+++ ol/dropbox.js
@@ -1,7 +1,15 @@
+var parsers;
 $(document).ready(function() {
+
+  var vector = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      projection: ol.proj.get('EPSG:4326')
+    })
+  });
 
   var map = new ol.Map({
     target: 'map',
+    renderer: ol.RendererHint.CANVAS,
     controls: ol.control.defaults({
       attribution: false,
       logo: false,
@@ -11,7 +19,8 @@ $(document).ready(function() {
       new ol.layer.TileLayer({
         visible: true,
         source: new ol.source.OSM()
-      })
+      }),
+      vector
     ],
     view: new ol.View2D({
       center: [0, 0],
@@ -20,17 +29,41 @@ $(document).ready(function() {
   });
 
   var projection = map.getView().getProjection();
-  var geojson = new ol.parser.GeoJSON();
+  parsers = {
+    'json': new ol.parser.GeoJSON(),
+    'gpx': new ol.parser.GPX(),
+    'kml': new ol.parser.KML()
+  };
 
   $('#db-chooser').on('DbxChooserSuccess', function(event) {
     var files = event.originalEvent.files;
     for (var i = 0; i < files.length; i++) {
       var file = files[i];
-      // console.log(file);
+      $("<div>loading " + file.name +"</div>").appendTo($('#db-chooser-info'));
+      // $('#db-chooser-info').append('loading ' + file.name);
+
       $.get(file.link, function(data) {
-	vector.parseFeatures(data, geojson, projection);
+        var parser = parsers[this.url.split('.').pop()];
+        vector.parseFeatures(data, parser, projection);
       });
     }
+  });
+
+  $('#vector-drop').bind('dragenter', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    $(event.target).addClass('active');
+  });
+  $('#vector-drop').bind('dragleave', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    $(event.target).removeClass('active');
+  });
+  $('.dropzone').bind('drop', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    var files = event.originalEvent.dataTransfer.files;
+    console.log(files);
   });
 
 });
