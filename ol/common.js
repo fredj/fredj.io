751b7cd52a23f3308aeef0d6a2221b9de13fea84
Frederic Junod <frederic.junod@camptocamp.com>
2013-07-16 17:04:10 +0200

    Snapshot

--- ol/common.js
+++ ol/common.js
@@ -9,11 +9,13 @@ var map = new ol.Map({
   layers: [
     new ol.layer.TileLayer({
       id: 'standard',
+      group: 'background',
       visible: true,
       source: new ol.source.OSM()
     }),
     new ol.layer.TileLayer({
       id: 'cycle',
+      group: 'background',
       visible: false,
       source: new ol.source.OSM({
         url: 'http://{a-c}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png'
@@ -21,6 +23,7 @@ var map = new ol.Map({
     }),
     new ol.layer.TileLayer({
       id: 'transport',
+      group: 'background',
       visible: false,
       source: new ol.source.OSM({
         url: 'http://{a-c}.tile.opencyclemap.org/transport/{z}/{x}/{y}.png'
@@ -28,6 +31,7 @@ var map = new ol.Map({
     }),
     new ol.layer.TileLayer({
       id: 'mapquest',
+      group: 'background',
       visible: false,
       source: new ol.source.MapQuestOSM()
     })
@@ -41,21 +45,57 @@ var map = new ol.Map({
 var target = $(map.getTarget());
 $(target.data('ol-map-target') || target).data('ol-map', map);
 
-$('[data-ol-bind-target^=layer]').each(function() {
-  var $this = $(this);
-  var target =  $this.data('ol-bind-target').split(':');
-  var property = $this.data('ol-bind-prop') || $this.closest('[data-ol-bind-prop]').data('ol-bind-prop');
-  var key = $this.prop('checked') == undefined ? 'value' : 'checked';
 
-  // FIXME: should start from property
-  var map = $this.parents().map(function() { return $(this).data('ol-map'); }).get(0);
+// "layer:visible:foobar"
+$(document).on('change.ol', '[data-ol-set^=layer]', function(event) {
+  var element = $(event.target);
+  var map = element.parents().map(function() { return $(this).data('ol-map'); }).get(0); // FIXME
 
-  var input = new ol.dom.Input($this.get(0));
+  var change = element.data('ol-set').split(':');
+  var property = change[1], layerId = change[2];
+  var value = element.prop('checked') === undefined ? element.prop('value') : element.prop('checked');
+  var group = element.prop('name');
 
   map.getLayers().forEach(function(layer) {
-    if (layer.get('id') === target[1]) {
-      input.bindTo(key, layer, property); // FIXME: memory leak ?
-      return;
+    if (layer.get('id') === layerId) {
+      layer.set(property, value);
+    } else {
+      console.log(layer);
+      if (layer.get('group') === group) {
+        // FIXME: only for boolean value
+        layer.set(property, !value);
+      }
     }
   });
 });
+
+
+$(document).on('click.ol', '[data-ol-set^=view]', function(event) {
+  var $this = $(this);
+  var change = $this.data('ol-set').split(':');
+  var prop = change[1], value = change[2];
+  var map = $this.parents().map(function() { return $(this).data('ol-map'); }).get(0); // FIXME
+  // ...
+});
+
+$(document).on('mousedown touchstart', '#wrapper.in', function(event) {
+  $(this).removeClass('in');
+});
+
+
+$('.typeahead.geonames').typeahead([{
+  name: 'geonames',
+  valueKey: 'name',
+  template: function(context) {
+    return '<div>' + context.name + '</div>';
+  },
+  remote: {
+    url: 'http://api.geonames.org/searchJSON?username=fredj&q=%QUERY',
+    filter: function(response) {
+      return response.geonames;
+    }
+  }
+}]).on('typeahead:selected', function() {
+
+});
+
