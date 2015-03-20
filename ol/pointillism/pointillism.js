66a9ea9c87119a89b3df7f59f6511aa6a69d2195
Frederic Junod <frederic.junod@camptocamp.com>
2015-03-20 09:51:45 +0100

    Refresh pointillism example

--- ol/pointillism/pointillism.js
+++ ol/pointillism/pointillism.js
@@ -1,41 +1,33 @@
-var ctx = $('<canvas>').get(0).getContext('2d');
+var size = 16;
 
 var map = new ol.Map({
   layers: [
     new ol.layer.Tile({
-      source: new ol.source.OSM({
-        tileLoadFunction: function(imageTile, src) {
-          var img = $(imageTile.getImage());
-          img.one('load', function() {
-            ctx.canvas.width = this.width;
-            ctx.canvas.height = this.height;
-            ctx.drawImage(this, 0, 0);
-
-            var size = 8, half_size = size / 2;
-            var imgd = ctx.getImageData(0, 0, this.width, this.height);
-            for (var x = 0; x < this.width; x += size) {
-              for (var y = 0; y < this.height; y += size) {
-                var idx = ((y * this.width) + x) * 4;
-                ctx.fillStyle = '#' + imgd.data[idx + 0].toString(16) + imgd.data[idx + 1].toString(16) + imgd.data[idx + 2].toString(16);
-                ctx.clearRect(x, y, size, size);
-                ctx.beginPath();
-                ctx.arc(x + half_size, y + half_size, half_size - 1, 0, Math.PI * 2, true);
-                ctx.closePath();
-                ctx.fill();
-              }
-            }
-            this.removeAttribute('crossorigin');
-            this.src = ctx.canvas.toDataURL();
-          });
-          img.attr('src', src);
-        }
-      })
+      source: new ol.source.OSM()
     })
   ],
-  renderer: ol.RendererHint.CANVAS,
   target: 'map',
-  view: new ol.View2D({
+  view: new ol.View({
     center: [-133839, 6971740],
     zoom: 17
   })
+});
+
+map.on('postcompose', function(event) {
+  var context = event.context;
+  var width = context.canvas.width;
+  var height = context.canvas.height;
+
+  var image = context.getImageData(0, 0, width, height).data;
+  for (var x = 0; x < width; x += size) {
+    for (var y = 0; y < height; y += size) {
+      var index = ((y * width) + x) * 4;
+      context.fillStyle = '#' + image[index + 0].toString(16) + image[index + 1].toString(16) + image[index + 2].toString(16);
+      context.clearRect(x, y, size, size);
+      context.beginPath();
+      context.arc(x + size / 2, y + size / 2, size / 2 - 1, 0, Math.PI * 2, true);
+      context.closePath();
+      context.fill();
+    }
+  }
 });
